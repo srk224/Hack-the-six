f4f4f690cde8e49cad8459233980eab082c156fe
Shahrukh Khan <abdulshahrukhkhan@Shahrukhs-MacBook-Pro.local>
2024-08-03 21:47:53 -0400

    API CHANGES

--- code-cure/open_ai_api.py
+++ code-cure/open_ai_api.py
@@ -1,24 +1,43 @@
 from openai import AzureOpenAI
 from dotenv import load_dotenv
 import os
+import json
+
+f = open('data.json')
+
+data = json.load(f)
 
 load_dotenv()
 client = AzureOpenAI(
-    api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
-    api_version="2024-02-01",
-    azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-    )
-    
-deployment_name='gpt-35-turbo' #This will correspond to the custom name you chose for your deployment when you deployed a model. Use a gpt-35-turbo-instruct deployment. 
-    
-# Send a completion call to generate an answer
-print('Sending a test completion job')
-start_phrase = '''Here is a python program. Please find the error and give me a suggestion as to how I might fix it:
+  api_key = os.getenv("AZURE_OPENAI_API_KEY"),  
+  api_version = "2024-02-01",
+  azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+)
+response = client.chat.completions.create(
+    model="gpt-35-turbo", 
+    messages=[
+        {"role": "system", "content": "Assistant is a large language model trained by OpenAI. You are supposed to Fix the code in error I will give you, please output the code only and not any text"},
+        {"role": "user", "content": f"I am gonna provide you some code which is as below {data["fileContent"]}. It has the following errors {data['errorMessage']}, with the following file structure {data['fileStructure']} Please, fix the Eroor and provide the correct code. Output the code only, no text."}
+    ]
+)
 
-print("hello world")
-test1 = [1, 2, 3]
+response_date = response.choices[0].message.content
+# print(response_date)
 
-for i in range (0, 4):
-    print(test1[i])'''
-response = client.completions.create(model=deployment_name, prompt=start_phrase, max_tokens=50)
-print(start_phrase+response.choices[0].text)
+start_delimiter = "```"
+end_delimiter = "```"
+
+start_index = response_date.find(start_delimiter)
+end_index = response_date.find(end_delimiter, start_index + len(start_delimiter))
+
+if start_index != -1 and end_index != -1:
+    start_index += len(start_delimiter)  
+    code_content = response_date[start_index:end_index].strip()
+    print(code_content)
+else:
+    print("No content found within triple backticks.")
+
+filename = 'response_data.txt'
+
+with open(filename, 'w') as file:
+    file.write(code_content)
